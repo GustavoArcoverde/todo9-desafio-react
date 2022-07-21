8e4f6cebce99749c2457a07945d7943c6c7bee2d
GustavoArcoverde <garcoverde3@gmail.com>
2022-07-21 16:20:32 -0300

    estiliza footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import S from './Footer.module.css'
+import React from "react";
+import S from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={S.footer}>
-        <p>Testando suas habilidades em HTML, CSS e JS.</p>
-        <p>Linx Impulse</p>
-        <p>2019</p>
-    </footer>
-  )
-}
+    <div className={S.gambiarra}>
+      <footer className={S.footer}>
+        <section className={S.content}>
+          <p>Testando suas habilidades em HTML, CSS e JS.</p>
+          <p>Linx Impulse</p>
+          <p>2019</p>
+        </section>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
